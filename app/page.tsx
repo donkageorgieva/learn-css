bcb06fb3bf0f8e3f19ac98b6a9db187bf8e98bb8
Donka <DoniGeorgieva16@gmail.com>
2023-09-24 11:07:57 +0300

    Layout links, font setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <nav>
+        <ul>
+          <li>
+            <Link href="quiz">Quiz</Link>
+          </li>
+          <li>
+            <Link href="practice">Practice</Link>
+          </li>
+        </ul>
+      </nav>
+    </main>
+  );
 }
