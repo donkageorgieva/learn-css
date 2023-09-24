b0e4289f858ddd87f4db1daa51413ecfd4609a57
Donka <DoniGeorgieva16@gmail.com>
2023-09-24 14:30:08 +0300

    Fetch all questions in server quiz component

--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -1,4 +1,15 @@
-const Quiz = () => {
+const Quiz = async () => {
+  let questions;
+  try {
+    const response = await fetch(`${process.env.localEnv}/api/questions`);
+
+    if (response.ok) {
+      questions = await response.json();
+    }
+  } catch (err) {
+    throw new Error("There was an error fetching the questions");
+  }
+
   return <></>;
 };
 
